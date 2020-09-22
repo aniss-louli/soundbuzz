a22ec81d7e977f3f93e1ab6fa3e651791cf12da3
aniss-louli <57408333+aniss-louli@users.noreply.github.com>
2020-09-22 16:59:36 +0200

    Update init_monitoring.sh

--- init_monitoring.sh
+++ init_monitoring.sh
@@ -31,7 +31,7 @@ sudo apt-get install \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 #Instal Docker-Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose 
